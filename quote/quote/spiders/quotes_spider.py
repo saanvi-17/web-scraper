211ba159c47c6aa47007558520e462b1ab29df95
saanvi <saanvijain1999@gmail.com>
2020-09-26 11:04:34 +0530

    first commit

---
+++ quote/quote/spiders/quotes_spider.py
@@ -0,0 +1,33 @@
+import scrapy
+from ..items import QuoteItem
+
+class QuoteSpider(scrapy.Spider):
+    name = 'quotes'
+    page_number = 2
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/'
+    ]
+
+    def parse(self, response):
+
+        items = QuoteItem()
+
+        all_div_quotes = response.css('div.quote')
+
+        for quotes in all_div_quotes:
+            title = quotes.css('.text::text').extract()
+            author = quotes.css('.author::text').extract()
+            tags = quotes.css('.tags .tag::text').extract()
+
+            items['title'] = title
+            items['author'] = author
+            items['tags'] = tags
+
+            yield items
+
+            next_page = 'http://quotes.toscrape.com/page/' + str(QuoteSpider.page_number) +'/'
+            if QuoteSpider.page_number <= 10:
+                QuoteSpider.page_number += 1
+                yield response.follow(next_page, callback=self.parse)
+
+
